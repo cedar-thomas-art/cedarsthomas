4fbeb6e3688bd15b8916c787fbef9ee1279f59ef
Tony Thomas <thoma127@umn.edu>
2019-06-29 09:21:36 -0500

    New dependency and getters for each letter

--- js/logo.js
+++ js/logo.js
@@ -10,6 +10,12 @@ class Logo {
     });
     this.originalColor = color(this.draw.attr().fill);
     this.rotateColors();
+
+    console.log(this.letterCPath);
+    console.log(this.letterEPath);
+    console.log(this.letterDPath);
+    console.log(this.letterAPath);
+    console.log(this.letterRPath);
   }
 
   rotateColors() {
@@ -18,7 +24,7 @@ class Logo {
         stroke: this.saturateColor(),
         fill: this.saturateColor()
       });
-      console.log(this.randomNumber);
+      // console.log(this.randomNumber);
       // console.log(this.mixColor("yellow"));
     },
     100)
@@ -30,6 +36,30 @@ class Logo {
 
   saturateColor() {
     return this.originalColor.saturate(this.randomNumber).hex();
+  }
+
+  path(index) {
+    return this.draw.children()[index].attr("d").split(",");
+  }
+
+  get letterCPath () {
+    return this.path(1);
+  }
+
+  get letterEPath () {
+    return this.path(2);
+  }
+
+  get letterDPath () {
+    return this.path(3);
+  }
+
+  get letterAPath () {
+    return this.path(4);
+  }
+
+  get letterRPath () {
+    return this.path(5);
   }
 
   get randomNumber() {
