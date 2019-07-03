21884febc5807eadf650f1c9d6f9758bf0727f52
Tony Thomas <thoma127@umn.edu>
2019-07-02 19:37:10 -0500

    Set up some templates

--- js/routing.js
+++ js/routing.js
@@ -1,19 +1,29 @@
 page = require('page');
+handlebars = require('handlebars');
 
 class Route {
   constructor() {
-    page('/', this.index);
-    page('/works', this.works);
-    page('/works/painting-drawing', this.paintingDrawing);
-    page('/works/sculpture', this.sculpture);
-    page('/works/photography', this.photography);
-    page('/contact', this.contact);
-    page('*', this.notFound);
+    this.render("template-nav", this.navElem);
+
+    page('/',                       () => { this.index() });
+    page('/works',                  () => { this.works() });
+    page('/works/painting-drawing', () => { this.paintingDrawing() });
+    page('/works/sculpture',        () => { this.sculpture() });
+    page('/works/photography',      () => { this.photography() });
+    page('/contact',                () => { this.contact() });
+    page('*',                       () => { this.notFound() });
     page();
   }
 
+  render(src, dest, ctx={}) {
+    const templateHtml = this.templateSrc(src);
+    const hb = handlebars.compile(templateHtml)
+    const compiled = hb(ctx);
+    dest.innerHTML = compiled;
+  }
+
   index() {
-    console.log("hello world");
+    this.render("template-home", this.homeElem);
   }
 
   works() {
@@ -38,6 +48,22 @@ class Route {
 
   notFound() {
     console.log("404 not found");
+  }
+
+  templateSrc(id) {
+    return document.getElementById(id).innerHTML;
+  }
+
+  getById(id) {
+    return document.getElementById(id);
+  }
+
+  get homeElem() {;
+    return this.getById("home-content");
+  }
+
+  get navElem() {
+    return document.getElementsByTagName("nav")[0];
   }
 }
 
